3266738431866f60241d29d3b67978cf1bf26e3c
Maxklos <herzognikolaus@gmail.com>
2016-03-25 21:48:31 +0100

    Cleaning and Fixing
    Fixing some Bugs
    Cleaning the Header

--- ReadData.py
+++ ReadData.py
@@ -6,6 +6,7 @@
 from time import *
 import time
 from Adafruit_BME280 import *
+import os
 
 sensor = BME280(mode=BME280_OSAMPLE_8)
 
@@ -16,9 +17,11 @@ hum = sensor.read_humidity()
 
 def writeDATA():
         #write year, month and day in the title
-	f = open('LOG' + strftime("%Y%m%d", time.localtime())'.txt', 'a')
+	f = open('LOG' + strftime("%Y%m%d", time.localtime()) + '.txt', 'a')
+	if os.stat('LOG' + strftime("%Y%m%d", time.localtime()) + '.txt').st_size == 0:
+		f.write("TEMP|PRESS|HUM\n")
 	f.write(time.strftime("%Y%m%d%H%M%S", time.localtime()) + '\n' + 
-                'TEMP {0:0} | PRESS {} | HUM {} \n'.format(temp, press, hum))
+                '{}|{}|{}\n'.format(temp, press, hum))
 	f.close()
 
 	
