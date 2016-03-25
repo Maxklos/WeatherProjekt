d9c1aca54d0769bc9bc90243ba188fecc83ec439
Maxklos <herzognikolaus@gmail.com>
2016-03-25 19:41:33 +0100

    Testing with Sensor
    Write Data to a LOG<date>.txt file

--- ReadData.py
+++ ReadData.py
@@ -1,16 +1,13 @@
 from time import *
 import time
-#from Adafruit_BME280 import *
+from Adafruit_BME280 import *
 
-#sensor = BME280(mode=BME280_OSAMPLE_8)
+sensor = BME280(mode=BME280_OSAMPLE_8)
 
-#temp = sensor.read_temperature()
-#pascals = sensor.read_pressure()
-#press = pascals / 100
-#hum = sensor.read_humidity()
-temp = '20'
-press = '30'
-hum = '40'
+temp = sensor.read_temperature()
+pascals = sensor.read_pressure()
+press = pascals / 100
+hum = sensor.read_humidity()
 
 def writeDATA():
 	f = open('LOG.txt', 'a')
