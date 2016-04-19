ed421b9f17dc87ef270d01d18ff9729b65db4dac
ChristopherKossatz <ChristopherKossatz>
2016-04-20 01:55:11 +0200

    bug fixes for client
    more stable, but i had to remove the server -> client com. in order to
    achieve this
    client -> server still working ;)

--- client/ReadData.py
+++ client/ReadData.py
@@ -9,10 +9,10 @@ from Adafruit_BME280 import *
 import os
 from client import sendData
 from client import installData
+#from client import recv_timeout
 import socket
 import sys
 import struct
-import time
 
 #sensor = BME280(mode=BME280_OSAMPLE_8)
 
@@ -28,41 +28,6 @@ def writeDATA(s):
 
         t = time.strftime("%Y%m%d%H%M%S", time.localtime())
         sendData(s,t, humidity = hum , pressure = press, temperature=temp)
-
-def recv_timeout(the_socket,timeout=2):
-    #make socket non blocking
-    the_socket.setblocking(0)
-
-    #total data partwise in an array
-    total_data=[];
-    data='';
-
-    #beginning time
-    begin=time.time()
-    while 1:
-        #if you got some data, then break after timeout
-        if total_data and time.time()-begin > timeout:
-            break
-
-        #if you got no data at all, wait a little longer, twice the timeout
-        elif time.time()-begin > timeout*2:
-            break
-
-        #recv something
-        try:
-            data = the_socket.recv(8192)
-            if data:
-                total_data.append(data)
-                #change the beginning time for measurement
-                begin=time.time()
-            else:
-                #sleep for sometime to indicate a gap
-                time.sleep(0.1)
-        except:
-            pass
-
-    #join all parts to make final string
-    return ''.join(total_data)
 
 #input ip
 host = "192.168.2.111"
@@ -89,24 +54,10 @@ print 'Socket Connected to ' + host + ' on ip ' + remote_ip
 
 try :
     #Set the whole string
-    installData(["humidity","temperature","pressure"],s)
-    m = recv_timeout(s)
-
-    if int(m) == 1:
-        print("succesdully installed parameters")
+    installData(s,["humidity","temperature","pressure"])
 
     for i in range(5):
     	writeDATA(s)
-        m = recv_timeout(s)
-
-        if int(m) == 1:
-            print("Send Data")
-        elif int(m) == -1:
-            print("Invalid Data")
-        else:
-            print("Could not transmit Data correctly")
-            sys.exit()
-
     	#900 = 15 min
     	time.sleep(2)
 except socket.error:
@@ -115,6 +66,6 @@ except socket.error:
     sys.exit()
 
 #get reply and print
-print recv_timeout(s)
+#print recv_timeout(s)
 
 s.close()
--- client/client.py
+++ client/client.py
@@ -1,15 +1,45 @@
-dnames = []
-
-def installData(n =[],s):
-    dnames = n
+def installData(s,n =[]):
     #check if all names are valid
     s.send(str(dnames))
 
 def sendData(s,t, **kwargs):
     #print kwargs
     #print '--------------------------'
-    s.send("time: {}".format(t))
-    for i in dnames:
+    s.send("time:{}".format(t))
+    for i in kwargs:
         s.send("{}:{}".format(i,kwargs[i]))
 
-    
+def recv_timeout(the_socket,timeout=2):     #doesn't work stable at all
+    #make socket non blocking
+    the_socket.setblocking(0)
+
+    #total data partwise in an array
+    total_data=[];
+    data='';
+
+    #beginning time
+    begin=time.time()
+    while 1:
+        #if you got some data, then break after timeout
+        if total_data and time.time()-begin > timeout:
+            break
+
+        #if you got no data at all, wait a little longer, twice the timeout
+        elif time.time()-begin > timeout*2:
+            break
+
+        #recv something
+        try:
+            data = the_socket.recv(8192)
+            if data:
+                total_data.append(data)
+                #change the beginning time for measurement
+                begin=time.time()
+            else:
+                #sleep for sometime to indicate a gap
+                time.sleep(0.1)
+        except:
+            pass
+
+    #join all parts to make final string
+    return ''.join(total_data)
