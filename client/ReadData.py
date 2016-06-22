38d4ef89b1e9d1da0675fd7138208674f068b76b
Maxklos <herzognikolaus@gmail.com>
2016-06-22 16:44:53 +0200

    Fixing the abort bug (resync)
    I somehow can't sync with the desktop client.... But now we fixed the bug that the Transmission would abort after 5 Pakets. 
    After 4 Transmissions any data send is writen to the database

--- client/ReadData.py
+++ client/ReadData.py
@@ -1,75 +1,126 @@
-#read data from a BME280 Sensor and write it into a LOG<date>.txt file
-#
-#author Benjamin Koderisch and Christopher Kossatz
-#version 1.3 21.04.2016
+#Server program
+#by Christopher Kossatz
+#0.2.6 21.04.16
 
-idpi = '0001'
 
-from time import *
-import time
-from Adafruit_BME280 import *
-import os
-from client import sendData
-from client import installData
-#from client import recv_timeout
 import socket
 import sys
-import struct
+from threading import Thread
+import sqlite3
 
-sensor = BME280(mode=BME280_OSAMPLE_8)
+HOST = '192.168.1.15'
+PORT = 8888
 
-def writeDATA(s):
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print 'Socket created'
+
+try:
+    s.bind((HOST, PORT))
+except socket.error , msg:
+    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
+
+print 'Socket bind complete'
+
+s.listen(10)
+print 'Socket now listening'
+
+def clientthread(conn):
+    #infinite loop
+    while True:
+        allData = []
+        #Sending message to connected client
+        #conn.send('1')
+
+        #3 data blocks
+        for i in range(12):
+
+            #Receiving from client
+            data = conn.recv(1024)
+            #reply = '1'
+            if not data:
+                break
+            allData.append(filter(None, data.split('\n')))
+            print data
+            #conn.send(reply)
+
+        print 'Ready with part one'
+
+        try:
+            sqconn = sqlite3.connect('WeatherProject.db')
+            sqc = sqconn.cursor()
+            sqc.execute("CREATE TABLE IF NOT EXISTS pi(time UNIX)")
+        except:
+            print "Couldn't connect to/ create DB, please try again"
+            sys.exit()
+
+        #fetch all Column Names
+        head = allData[0][0].split('-')
+        del allData[0]
+        print head[-1]    # could be used to select table in database
+        del head[-1]
+
+        #fetch witch columns are already present
+        try:
+            sqc.execute('PRAGMA table_info(pi)')
+            d = sqc.fetchall()
+        except:
+            print "Couldn't fetch Data from db"
+        present_columns = []
+
+        for i in d:
+            present_columns.append(i[1])
+        #test
+        print("hallo")
+        #insert all columns that aren't present yet
+        try:
+            for h in head:
+                if h not in present_columns:
+                    sqc.execute('ALTER TABLE pi ADD COLUMN {} REAL'.format(h))
+                    sqconn.commit()
+        except:
+            print "Couldn't insert additional columns!"
+
+        #formating Data
+        r = []
+        allData_new = []
+        i = 0
+
+        for row in allData:
+            for cell in row:
+                cell = cell.split(':')
+                if ((cell[0] == 'time') & (len(r) == 0)) | ((i <= len(head)) & (cell[0] != 'time')):
+                    r.append(cell)
+                elif (cell[0] == 'time'):
+                    allData_new.append(r)
+                    r = []
+                    r.append(cell)
+
+        #insert Data into table
+        try:
+            for data in allData_new:
+                sqc.execute("INSERT INTO pi (time) VALUES ({})".format(data[0][1]))
+                t = data[0][1]
+                del data[0]
+                for cell in data:
+                    sqc.execute("UPDATE pi set {} = {} WHERE time = {}".format(cell[0],cell[1],t))
+                    sqconn.commit()
+        except:
+            print 'Database is probably locked. PLease close all other programms that might interfere with the db!'
+
+        sqconn.close()
+        conn.close()
+
+#now keep talking with the client
+for i in range(2):
+    #wait to accept a connection
+    conn, addr = s.accept()
+    print 'Connected with ' + addr[0] + ':' + str(addr[1])
+
+    #start new thread
     try:
-        temp = sensor.read_temperature()
-        pascals = sensor.read_pressure()
-        press = pascals / 100
-        hum = sensor.read_humidity()
+        t = Thread(target = clientthread ,args = (conn,)).start()
     except:
-        print 'Sensor Error. Please try again.'
-    #hum = 50
-    #press = 1000
-    #temp = 85.3
-    print str(temp)
-    print str(press)
-    print str(hum)
-
-    t = time.strftime("%Y%m%d%H%M%S", time.localtime())
-    sendData(s,t, humidity = hum , pressure = press, temperature=temp)
-
-#input ip
-host = "192.168.1.191"
-port = 8888
-
-#create an INET, STREAMing socket
-try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-except socket.error:
-    print 'Failed to create socket'
-    sys.exit()
-
-print 'Socket Created'
-
-try:
-    remote_ip = socket.gethostbyname( host )
-    s.connect((host, port))
-
-except socket.gaierror:
-    print 'Hostname could not be resolved. Exiting'
-    sys.exit()
-
-print 'Socket Connected to ' + host + ' on ip ' + remote_ip
-
-try :
-    #Set the whole string
-    installData(s,idpi,["humidity","temperature","pressure"])
-
-    for i in range(5):
-    	writeDATA(s)
-    	#900 = 15 min
-    	time.sleep(20)
-except socket.error:
-    #Send failed
-    print 'Send failed'
-    sys.exit()
+        print "Couldn't start Thread"
 
 s.close()
