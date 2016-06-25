b262c76d6c8d73b30586abed60b3c2b0a7bd8fec
Maxklos <herzognikolaus@gmail.com>
2016-06-25 20:37:55 +0200

    Fixing the Bug AGAIN
    Because Github....

--- server/server.py
+++ server/server.py
@@ -8,7 +8,7 @@ import sys
 from threading import Thread
 import sqlite3
 
-HOST = '192.168.1.191'
+HOST = '192.168.1.15'
 PORT = 8888
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -31,79 +31,81 @@ def clientthread(conn):
     #conn.send('1')
 
     #infinite loop so that function do not terminate and thread do not end.
-    for i in range(17):
+    while(True):
+        
+        for i in range(17):
 
-        #Receiving from client
-        data = conn.recv(1024)
-        #reply = '1'
-        if not data:
-            break
-        allData.append(filter(None, data.split('\n')))
-        print data
-        #conn.send(reply)
+            #Receiving from client
+            data = conn.recv(1024)
+            #reply = '1'
+            if not data:
+                break
+            allData.append(filter(None, data.split('\n')))
+            print data
+            #conn.send(reply)
 
-    print 'Ready with part one'
+        print 'Ready with part one'
 
-    try:
-        sqconn = sqlite3.connect('WeatherProject.db')
-        sqc = sqconn.cursor()
-        sqc.execute("CREATE TABLE IF NOT EXISTS pi(time UNIX)")
-    except:
-        print "Couldn't connect to/ create DB, please try again"
-        sys.exit()
+        try:
+            sqconn = sqlite3.connect('WeatherProject.db')
+            sqc = sqconn.cursor()
+            sqc.execute("CREATE TABLE IF NOT EXISTS pi(time UNIX)")
+        except:
+            print "Couldn't connect to/ create DB, please try again"
+            sys.exit()
 
-    #fetch all Column Names
-    head = allData[0][0].split('-')
-    del allData[0]
-    print head[-1]    # could be used to select table in database
-    del head[-1]
+        #fetch all Column Names
+        head = allData[0][0].split('-')
+        del allData[0]
+        print head[-1]    # could be used to select table in database
+        del head[-1]
 
-    #fetch witch columns are already present
-    try:
-        sqc.execute('PRAGMA table_info(pi)')
-        d = sqc.fetchall()
-    except:
-        print "Couldn't fetch Data from db"
-    present_columns = []
+        #fetch witch columns are already present
+        try:
+            sqc.execute('PRAGMA table_info(pi)')
+            d = sqc.fetchall()
+        except:
+            print "Couldn't fetch Data from db"
+        present_columns = []
 
-    for i in d:
-        present_columns.append(i[1])
+        for i in d:
+            present_columns.append(i[1])
 
-    #insert all columns that aren't present yet
-    try:
-        for h in head:
-            if h not in present_columns:
-                sqc.execute('ALTER TABLE pi ADD COLUMN {} REAL'.format(h))
-                sqconn.commit()
-    except:
-        print "Couldn't insert additional columns!"
+        #insert all columns that aren't present yet
+        try:
+            for h in head:
+                if h not in present_columns:
+                    sqc.execute('ALTER TABLE pi ADD COLUMN {} REAL'.format(h))
+                    sqconn.commit()
+        except:
+            print "Couldn't insert additional columns!"
 
-    #formating Data
-    r = []
-    allData_new = []
-    i = 0
+        #formating Data
+        r = []
+        allData_new = []
+        i = 0
 
-    for row in allData:
-        for cell in row:
-            cell = cell.split(':')
-            if ((cell[0] == 'time') & (len(r) == 0)) | ((i <= len(head)) & (cell[0] != 'time')):
-                r.append(cell)
-            elif (cell[0] == 'time'):
-                allData_new.append(r)
-                r = []
-                r.append(cell)
+        for row in allData:
+            for cell in row:
+                cell = cell.split(':')
+                if ((cell[0] == 'time') & (len(r) == 0)) | ((i <= len(head)) & (cell[0] != 'time')):
+                    r.append(cell)
+                elif (cell[0] == 'time'):
+                    allData_new.append(r)
+                    r = []
+                    r.append(cell)
 
-    #insert Data into table
-    try:
-        for data in allData_new:
-            sqc.execute("INSERT INTO pi (time) VALUES ({})".format(data[0][1]))
-            t = data[0][1]
-            del data[0]
-            for cell in data:
-                sqc.execute("UPDATE pi set {} = {} WHERE time = {}".format(cell[0],cell[1],t))
-                sqconn.commit()
-    except:
-        print 'Database is probably locked. PLease close all other programms that might interfere with the db!'
+        #insert Data into table
+        try:
+            for data in allData_new:
+                sqc.execute("INSERT INTO pi (time) VALUES ({})".format(data[0][1]))
+                t = data[0][1]
+                del data[0]
+                for cell in data:
+                    sqc.execute("UPDATE pi set {} = {} WHERE time = {}".format(cell[0],cell[1],t))
+                    sqconn.commit()
+        except:
+            print 'Database is probably locked. PLease close all other programms that might interfere with the db!'
 
     sqconn.close()
     conn.close()
